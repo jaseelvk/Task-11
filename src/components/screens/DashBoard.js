8fceb4f2609c2af76f5fa42df58897ce1c1fd73e
jaseelvk <jaseelvk0@gmail.com>
2023-09-06 16:25:54 +0530

    second-commit

--- src/components/screens/DashBoard.js
+++ src/components/screens/DashBoard.js
@@ -28,7 +28,6 @@ function DashBoard() {
               </Total>
             </TotelContainer>
           </MaleContainer>
-
           <FemaleContainer>
             <GuestContainer>
               <ImgContainer>
